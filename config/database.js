24fd116bedc4d5bd084410968d81bb23c2c1fd38
scottie-schneider <scott@laclave.co>
2018-06-21 15:32:47 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://scott@warcat.co:speje33ma*@ds163870.mlab.com:63870/facebook_submission_app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
