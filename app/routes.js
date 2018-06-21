e201e50c87133a35d21776091fce7caf0e1e1579
scottie-schneider <scott@laclave.co>
2018-06-21 15:44:18 -0500

    view engine

--- app/routes.js
+++ app/routes.js
@@ -5,8 +5,7 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.send('hello world!');
-        //res.render('index.ejs'); // load the index.ejs file
+        res.render('index.ejs'); // load the index.ejs file
     });
 
     // =====================================
