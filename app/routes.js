5177081725a6200399e8c98779900c32647e91df
scottie-schneider <scott@laclave.co>
2018-06-21 15:40:04 -0500

    sanity check

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,8 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.send('hello world!');
+        //res.render('index.ejs'); // load the index.ejs file
     });
 
     // =====================================
