985e268e7e88082a8f6ec3f51a245e325c108131
Scottie Schneider <scott@laclave.co>
2018-07-04 22:05:01 -0500

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : '1721479064638260', // your App ID
-        'clientSecret'  : 'ce617e52626f371be828ac6e3da6c97f', // your App Secret
+        'clientID'      : // your App ID
+        'clientSecret'  : // your App Secret
         'callbackURL'   : 'https://radiant-atoll-16957.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
